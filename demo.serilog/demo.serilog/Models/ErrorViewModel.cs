12547be87f3becbec918f458678a61d1f42586a4
Son Nguyen <firefumavn@gmail.com>
2021-03-13 11:22:37 +0700

    Add project

---
+++ demo.serilog/demo.serilog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demo.serilog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
