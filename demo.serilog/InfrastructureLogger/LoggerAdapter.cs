08578284a0caf0cf70ccac3b93ea11cbb1206bf4
Son Nguyen <khacson1610@gmail.com>
2021-03-13 17:45:09 +0700

    Add Logs

---
+++ demo.serilog/InfrastructureLogger/LoggerAdapter.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfrastructureLogger
+{
+    public static class LoggerAdapter
+    {
+        private static IConfiguration _configuration;
+        public static IConfiguration Configuration
+        {
+            set
+            {
+                if (_configuration == null)
+                {
+                    _configuration = value;
+                }
+            }
+        }
+        /// <summary>
+        /// Debug
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Debug(string message)
+        {
+            Log.Debug(message);
+        }
+        /// <summary>
+        /// Debug
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="propertyValue"></param>
+        public static void Debug<T>(string message, T propertyValue)
+        {
+            Log.Debug<T>(message, propertyValue);
+        }
+
+        /// <summary>
+        /// Error
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Error(string message)
+        {
+            Log.Error(message);
+        }
+
+        /// <summary>
+        /// Error
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="propertyValue"></param>
+        public static void Error<T>(string message, T propertyValue)
+        {
+            Log.Error<T>(message, propertyValue);
+        }
+
+        /// <summary>
+        /// Fatal
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Fatal(string message)
+        {
+            Log.Fatal(message);
+        }
+
+        /// <summary>
+        /// Fatal
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="propertyValue"></param>
+        public static void Fatal<T>(string message, T propertyValue)
+        {
+            Log.Fatal<T>(message, propertyValue);
+        }
+
+        /// <summary>
+        /// Information
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Information(string message)
+        {
+            Log.Information(message);
+        }
+
+        /// <summary>
+        /// Information
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="propertyValue"></param>
+        public static void Information<T>(string message, T propertyValue)
+        {
+            Log.Information<T>(message, propertyValue);
+        }
+
+        /// <summary>
+        /// Warning
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Warning(string message)
+        {
+            Log.Warning(message);
+        }
+
+        /// <summary>
+        /// Warning
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="message"></param>
+        /// <param name="propertyValue"></param>
+        public static void Warning<T>(string message, T propertyValue)
+        {
+            Log.Warning<T>(message, propertyValue);
+        }
+    }
+}
---
+++ demo.serilog/InfrastructureLogger/LoggerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+
+namespace InfrastructureLogger
+{
+    public static class LoggerConfiguration
+    {
+		public static ILoggerFactory AddLoggerFactory(this ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddSerilog();
+
+			return loggerFactory;
+		}
+
+		/// <summary>
+		/// CreateLogger
+		/// </summary>
+		/// <param name="configuration"></param>
+		public static void CreateLogger(this IConfiguration configuration)
+		{
+			Log.Logger = new Serilog.LoggerConfiguration()
+				.ReadFrom.Configuration(configuration)
+				.CreateLogger();
+		}
+	}
+}
